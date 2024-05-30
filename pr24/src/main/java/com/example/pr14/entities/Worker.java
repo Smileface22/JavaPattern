de05d79db1e1a557e365ce88a6c36d7655113bc8
Smileface22 <149334736+Smileface22@users.noreply.github.com>
2024-05-30 12:06:09 +0300

    23-24

---
+++ pr24/src/main/java/com/example/pr14/entities/Worker.java
@@ -0,0 +1,32 @@
+package com.example.pr14.entities;
+
+public class Worker {
+    private String firstName;
+    private String lastName;
+    private String middleName;
+
+    // Getters and Setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+}
